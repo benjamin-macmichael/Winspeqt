cd3cb340ae904b719f45b98dc01f709b775b0d73
prestonfitz <prestonfitz@gmail.com>
2026-01-23 19:17:56 -0700

    feat: link page to various apps on computer

--- Winspeqt/Views/Monitoring/StartupImpactPage.xaml.cs
+++ Winspeqt/Views/Monitoring/StartupImpactPage.xaml.cs
@@ -1,5 +1,7 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Diagnostics;
 using Winspeqt.ViewModels.Monitoring;
 
 namespace Winspeqt.Views.Monitoring
@@ -20,6 +22,77 @@ namespace Winspeqt.Views.Monitoring
             if (Frame.CanGoBack)
             {
                 Frame.GoBack();
+            }
+        }
+
+
+        private void OpenStartupSettings_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Open Windows Update settings
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "ms-settings:startupapps",
+                    UseShellExecute = true
+                });
+            }
+            catch (System.Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error opening startup apps: {ex.Message}");
+            }
+        }
+
+        private void OpenRegedit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Open Windows Update settings
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "regedit",
+                    UseShellExecute = true
+                });
+            }
+            catch (System.Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error opening Regedit: {ex.Message}");
+            }
+        }
+
+        private void OpenTaskScheduler_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Open Windows Update settings
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "taskschd.msc",
+                    UseShellExecute = true
+                });
+            }
+            catch (System.Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error opening task scheduler: {ex.Message}");
+            }
+        }
+
+        private void OpenStartupFolder_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string startupPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "explorer.exe",
+                    Arguments = startupPath,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error opening Startup folder: {ex.Message}");
             }
         }
     }
