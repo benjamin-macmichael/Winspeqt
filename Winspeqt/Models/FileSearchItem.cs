c7e1d0886dfe1479e736b302b5701bdbd6efcaf9
prestonfitz <prestonfitz@gmail.com>
2026-02-07 15:44:25 -0700

    feat: create model for search items

---
+++ Winspeqt/Models/FileSearchItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winspeqt.Models
+{
+    public class FileSearchItem
+    {
+        string Name { get; }
+        string FilePath { get; }
+        string Type { get; }
+
+        public FileSearchItem (string name, string path, string type)
+        {
+            Name = name;
+            FilePath = path;
+            Type = type;
+        }
+    }
+}
--- Winspeqt/ViewModels/Optimization/LargeFileFinderViewModel.cs
+++ Winspeqt/ViewModels/Optimization/LargeFileFinderViewModel.cs
@@ -1,35 +1,50 @@
 ﻿using Microsoft.UI.Xaml.Shapes;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Winspeqt.Helpers;
+using Winspeqt.Models;
 
 namespace Winspeqt.ViewModels.Optimization
 {
-    public class LargeFileFinderViewModel
+    public class LargeFileFinderViewModel : ObservableObject
     {
-        public LargeFileFinderViewModel ()
+        public ObservableCollection<FileSearchItem> FolderItems { get; set; }
+        public LargeFileFinderViewModel()
         {
             string initialFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 
             System.Diagnostics.Debug.WriteLine(initialFolder);
 
-            string[] folders = FindFilesForFolder(initialFolder);
+            FolderItems = new ObservableCollection<FileSearchItem>(FindFilesForFolder(initialFolder));
 
-            foreach (var folder in folders)
+            foreach (var folder in FolderItems)
             {
                 System.Diagnostics.Debug.WriteLine(folder);
             }
         }
 
-        string[] FindFilesForFolder(string folder)
+        List<FileSearchItem> FindFilesForFolder(string folder)
         {
-            string[] folders = Directory.GetDirectories(folder);
-            string[] files = Directory.GetFiles(folder);
+            List<FileSearchItem> folders = PathToObject(Directory.GetDirectories(folder), "folder");
+            List<FileSearchItem> files = PathToObject(Directory.GetFiles(folder), "file");
 
             return [..folders, ..files];
+        }
+
+        List<FileSearchItem> PathToObject(string[] paths, string type)
+        {
+            List<FileSearchItem> temp = [];
+            foreach (var path in paths)
+            {
+                temp.Add(new FileSearchItem("", path, type));
+            }
+
+            return temp;
         }
     }
 }
