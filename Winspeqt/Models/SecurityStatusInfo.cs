236963f9a8d5c64b1ec82c00ff5b39ad732493ea
prestonfitz <prestonfitz@gmail.com>
2026-02-19 20:32:37 -0700

    fix: processinfo, securitystatusinfo

--- Winspeqt/Models/SecurityStatusInfo.cs
+++ Winspeqt/Models/SecurityStatusInfo.cs
@@ -2,20 +2,20 @@
 {
     public class SecurityStatusInfo
     {
-        public SecurityComponentStatus WindowsDefenderStatus { get; set; }
-        public SecurityComponentStatus FirewallStatus { get; set; }
-        public SecurityComponentStatus WindowsUpdateStatus { get; set; }
-        public SecurityComponentStatus BitLockerStatus { get; set; }
+        public SecurityComponentStatus WindowsDefenderStatus { get; set; } = new SecurityComponentStatus();
+        public SecurityComponentStatus FirewallStatus { get; set; } = new SecurityComponentStatus();
+        public SecurityComponentStatus WindowsUpdateStatus { get; set; } = new SecurityComponentStatus();
+        public SecurityComponentStatus BitLockerStatus { get; set; } = new SecurityComponentStatus();
         public int OverallSecurityScore { get; set; }
-        public string OverallStatus { get; set; }
+        public string OverallStatus { get; set; } = "";
     }
 
     public class SecurityComponentStatus
     {
-        public bool IsEnabled { get; set; }
-        public string Status { get; set; }
-        public string Message { get; set; }
-        public string Icon { get; set; }
-        public string Color { get; set; }
+        public bool IsEnabled { get; set; } = false;
+        public string Status { get; set; } = "";
+        public string Message { get; set; } = "";
+        public string Icon { get; set; } = "";
+        public string Color { get; set; } = "";
     }
 }
