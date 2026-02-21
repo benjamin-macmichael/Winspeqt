89499fa1dc7be4b8d2d17a4a4195a61e6e3361f4
benjamin-macmichael <benjaminmac21@gmail.com>
2026-02-20 18:20:18 -0700

    add global settings page

---
+++ Winspeqt/Models/SettingsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Winspeqt.Models
+{
+    public class SettingsModel
+    {
+        public bool LaunchAtStartup { get; set; } = true;
+        // Future settings can be added here
+        // e.g. public bool WeeklyAppScanEnabled { get; set; } = false;
+        // e.g. public bool HealthScoreNotificationsEnabled { get; set; } = true;
+    }
+}
---
+++ Winspeqt/Services/SettingsService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.Storage;
+using Winspeqt.Models;
+
+namespace Winspeqt.Services
+{
+    public class SettingsService
+    {
+        private readonly ApplicationDataContainer _localSettings;
+
+        public SettingsService()
+        {
+            _localSettings = ApplicationData.Current.LocalSettings;
+        }
+
+        public SettingsModel LoadSettings()
+        {
+            return new SettingsModel
+            {
+                LaunchAtStartup = GetSetting("LaunchAtStartup", true),
+            };
+        }
+
+        public void SaveSettings(SettingsModel settings)
+        {
+            SetSetting("LaunchAtStartup", settings.LaunchAtStartup);
+        }
+
+        public bool GetSetting(string key, bool defaultValue)
+        {
+            try
+            {
+                if (_localSettings.Values.ContainsKey(key))
+                    return (bool)_localSettings.Values[key];
+                return defaultValue;
+            }
+            catch { return defaultValue; }
+        }
+
+        public void SetSetting(string key, bool value)
+        {
+            try
+            {
+                _localSettings.Values[key] = value;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error saving setting '{key}': {ex.Message}");
+            }
+        }
+    }
+}
